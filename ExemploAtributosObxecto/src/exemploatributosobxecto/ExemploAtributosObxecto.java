13c1f3ee4b490fad66fa66fc3c2f22c8aea63d0f
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-12-12 09:14:49 +0100

    Codigo Limpio

---
+++ ExemploAtributosObxecto/src/exemploatributosobxecto/ExemploAtributosObxecto.java
@@ -0,0 +1,10 @@
+package exemploatributosobxecto;
+
+public class ExemploAtributosObxecto {
+
+    public static void main(String[] args) {
+        Conta obxConta = new Conta("millonetis","11111111", 1000000);
+        System.out.println(obxConta.toString());
+    }
+    
+}
